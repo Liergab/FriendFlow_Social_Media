efe76b89aa4a6ab4d8921734d8efff2cb92685cf
BRYAN <bryangabrielberja25@gmail.com>
2023-10-21 18:37:25 +0800

    update_dummyData_to_Api

--- client/src/components/Online/OnlineFrnd.jsx
+++ client/src/components/Online/OnlineFrnd.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
-
-import frpic from '../../assets/image/frpic.jpeg';
 import { Avatar,Badge} from '@material-tailwind/react';
+import {Link} from 'react-router-dom'
 const OnlineFrnd = ({user}) => {
   return (
     <div className='space-x-2'>
     <Badge color='green'>
-      <Avatar src={frpic} size='sm' />
+      <Link to={`/profile/${user?.username}`}>
+      {user?.profilePicture
+        ? <Avatar src={`http://localhost:8001/images/${user?.profilePicture}`} size='sm' /> 
+        : null
+      }
+      </Link>
     </Badge>
     <span>{user?.username}</span>
   </div>
