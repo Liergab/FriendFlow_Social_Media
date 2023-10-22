1bc2137a76c38478801271448de8561aa71c9324
BRYAN <bryangabrielberja25@gmail.com>
2023-10-22 21:47:39 +0800

    'include_the_password'

--- server/middleware/protected.js
+++ server/middleware/protected.js
@@ -10,7 +10,7 @@ const protect = asyncHandler(async(req,res,next) => {
             token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.SECRET_KEY)
 
-            req.user = await User.findById(decoded.id).select('-password')
+            req.user = await User.findById(decoded.id)
 
             next()
         } catch (error) {
