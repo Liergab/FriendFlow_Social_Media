6a04142ddbb649df706b557ed7a9721a5c20fddd
BRYAN <bryangabrielberja25@gmail.com>
2023-10-22 21:46:39 +0800

    'add_update_button'

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
 import Feed from "../../components/Feed"
 import Rightbar from "../../components/Rightbar"
 import Sidebar from "../../components/Sidebar"
+import Update from "../../components/userUpdate/Update";
 import { ApiGetUser } from "../../hooks/useUserApi";
 import Topbar from "../topbar/Topbar"
 import { useQuery } from "@tanstack/react-query";
@@ -26,12 +26,23 @@ const Profile = () => {
             <div className="flex flex-col flex-[9]">
               <div>
                 <div className="h-80 relative">
-                  <img src={`http://localhost:8001/images/${user?.coverPicture}`} alt="" className="w-full h-64 z-0"/>
-                   <img src={`http://localhost:8001/images/${user?.profilePicture} `} alt="" className=" h-40 w-40 rounded-full object-cover absolute  left-0 right-0 top-40 bottom-0 m-auto border-4 border-white" />
-                </div>
+                  {user?.coverPicture 
+                    ? <img src={`http://localhost:8001/images/${user?.coverPicture}`} alt="" className="w-full h-64 z-0"/> 
+                    : null
+                  }
+                  {user?.profilePicture 
+                    ? <img src={`http://localhost:8001/images/${user?.profilePicture} `} alt="" className=" h-40 w-40 rounded-full object-cover absolute  left-0 right-0 top-40 bottom-0 m-auto border-4 border-white" />
+                    : null
+                  }
+                  </div>
                 <div className="flex flex-col items-center mt-4">
                   <h1 className="font-bold text-2xl text-gray-800">{user?.username}</h1>
                   <h1 className="text-gray-800">{user?.decs}</h1>
+                  {reqUser?.username === user?.username && (
+                    <div>
+                      <Update/>
+                    </div>
+                  )}
                 </div>
               </div>
               <div className="flex">
